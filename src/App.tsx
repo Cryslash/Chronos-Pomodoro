fee9b1e08256bd293fa9cb318bf3177ae8e79edf
Crystofher Lins <crystofher_lins@outlook.com>
2025-05-12 00:00:02 -0300

    Context and models added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import './styles/global.css';
 import './styles/theme.css';
 
 import { Home } from './pages/Home';
+import { TaskConextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskConextProvider>
+      <Home />
+    </TaskConextProvider>
+  );
 }
